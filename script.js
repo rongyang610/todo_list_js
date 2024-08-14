199303218111ee0ae4d8352950210cd2c37fa9de
rongyang610 <rongyang610@gmail.com>
2024-08-14 00:18:59 -0500

    feat(js): add the js file to handle the todo list logic

---
+++ script.js
@@ -0,0 +1,81 @@
+const todos = [
+  {
+    text: "Do nothing",
+    completed: false,
+  },
+  {
+    text: "Make a todo list",
+    completed: false,
+  },
+  {
+    text: "get a job",
+    completed: false,
+  },
+  {
+    text: "algo",
+    completed: false,
+  },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("todo-form");
+  const list = document.getElementById("todo-list");
+  const submitButton = document.getElementById("submit-button");
+  const todoInput = document.getElementById("todo-input");
+
+  const toggleSubmitButton = () => {
+    if (todoInput.value.trim() === "") {
+      submitButton.disabled = true;
+    } else {
+      submitButton.disabled = false;
+    }
+  };
+
+  toggleSubmitButton();
+
+  todoInput.addEventListener("input", toggleSubmitButton);
+  todoInput.focus();
+
+  const addListItem = ({ text, completed = false }) => {
+    const listItem = document.createElement("li");
+
+    const listItemText = document.createElement("span");
+    listItemText.textContent = text;
+
+    const listItemCheckbox = document.createElement("input");
+    listItemCheckbox.type = "checkbox";
+    listItemCheckbox.checked = completed;
+    listItemCheckbox.setAttribute("aria-label", `Mark ${text} as complete`);
+    listItemCheckbox.addEventListener("change", () => {
+      if (listItemCheckbox.checked) {
+        listItemCheckbox.setAttribute(
+          "aria-label",
+          `Mark ${text} as incomplete`
+        );
+      } else {
+        listItemCheckbox.setAttribute("aria-label", `Mark ${text} as complete`);
+      }
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+    deleteButton.setAttribute("aria-label", `Delete ${text}`);
+    deleteButton.addEventListener("click", () => {
+      list.removeChild(listItem);
+    });
+
+    listItem.appendChild(listItemCheckbox);
+    listItem.appendChild(listItemText);
+    listItem.appendChild(deleteButton);
+
+    list.appendChild(listItem);
+  };
+
+  todos.forEach(addListItem);
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addListItem({ text: todoInput.value.trim() });
+    form.reset();
+  });
+});
